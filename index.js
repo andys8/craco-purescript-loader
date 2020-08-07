270787c7909cbb0b1cdd204a21cd27ec17b4612d
andys8 <andys8@users.noreply.github.com>
2020-08-07 14:02:38 +0200

    PureScript loader plugin

---
+++ index.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const { addBeforeLoader, loaderByName, getLoader } = require("@craco/craco");
+
+// Detect watch
+// <https://github.com/purescript/spago#get-started-from-scratch-with-webpack-frontend-projects>
+const isWatch = process.argv.some((a) => a === "--watch");
+const isWebpackDevServer = process.argv.some(
+  (a) => path.basename(a) === "webpack-dev-server"
+);
+
+const throwError = (message, githubIssueQuery) =>
+  throwUnexpectedConfigError({
+    packageName: "craco-less",
+    githubRepo: "ndbroadbent/craco-less",
+    message,
+    githubIssueQuery,
+  });
+
+module.exports = {
+  overrideWebpackConfig: ({ webpackConfig }) => {
+    // Resolve purescript extension
+    if (
+      !webpackConfig ||
+      !webpackConfig.resolve ||
+      !webpackConfig.resolve.extensions ||
+      typeof webpackConfig.resolve.extensions !== "object"
+    ) {
+      throwError("No valid webpackConfig.resolve.extensions");
+    }
+    webpackConfig.resolve.extensions.push(".purs");
+
+    // Allow imports outside of `src` folder for purescript dependencies
+    if (
+      !webpackConfig ||
+      !webpackConfig.resolve ||
+      !webpackConfig.resolve.plugins ||
+      typeof webpackConfig.resolve.plugins !== "object"
+    ) {
+      throwError("No valid webpackConfig.resolve.plugins");
+    }
+    webpackConfig.resolve.plugins = webpackConfig.resolve.plugins.filter(
+      ({ constructor }) =>
+        !constructor || constructor.name !== "ModuleScopePlugin"
+    );
+
+    // PureScript loader
+    const pursLoader = {
+      loader: "purs-loader",
+      test: /\.purs$/,
+      exclude: /node_modules/,
+      query: {
+        spago: true,
+        pscIde: true,
+        watch: isWebpackDevServer || isWatch,
+      },
+    };
+
+    // Append purs-loader before file-loader
+    const fileLoader = loaderByName("file-loader");
+    const { isFound } = getLoader(webpackConfig, fileLoader);
+    if (!isFound) {
+      throw new Error("Didn't find expected 'file-loader'");
+    }
+    addBeforeLoader(webpackConfig, fileLoader, pursLoader);
+
+    return webpackConfig;
+  },
+};
